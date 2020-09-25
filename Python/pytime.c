436ed3214996c910178d6568c091af8a47591826
Kevin Adler <kadler@us.ibm.com>
2020-09-25 15:25:14 -0500

    Disable clock_getres on IBM i
    clock_getres syscall is not supported by SLIC up through IBM i 7.3
    
    Just hard-code the actual resolution for now. Should double check
    that the resolution is the same for both monotonic and realtime clocks.

--- Python/pytime.c
+++ Python/pytime.c
@@ -612,10 +612,15 @@ pygettimeofday(_PyTime_t *tp, _Py_clock_info_t *info, int raise)
         info->implementation = "clock_gettime(CLOCK_REALTIME)";
         info->monotonic = 0;
         info->adjustable = 1;
-        if (clock_getres(CLOCK_REALTIME, &res) == 0)
+#ifdef __PASE__
+        info->resolution = 1e-3;
+#else
+        if (clock_getres(CLOCK_REALTIME, &res) == 0) {
             info->resolution = res.tv_sec + res.tv_nsec * 1e-9;
         else
             info->resolution = 1e-9;
+        }
+#endif
     }
 #else   /* HAVE_CLOCK_GETTIME */
 
@@ -752,11 +757,15 @@ pymonotonic(_PyTime_t *tp, _Py_clock_info_t *info, int raise)
         info->monotonic = 1;
         info->implementation = implementation;
         info->adjustable = 0;
+#ifdef __PASE__
+        info->resolution = 1e-3;
+#else
         if (clock_getres(clk_id, &res) != 0) {
             PyErr_SetFromErrno(PyExc_OSError);
             return -1;
         }
         info->resolution = res.tv_sec + res.tv_nsec * 1e-9;
+#endif
     }
     if (_PyTime_FromTimespec(tp, &ts, raise) < 0)
         return -1;
