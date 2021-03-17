18e2318d13734dade053b835ba2f35b02b69aeab
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2021-03-16 22:38:57 -0500

    test: Skip itimer_prof and itimer_virtual
    Both are ITIMER_PROF ITIMER_VIRTUAL are unsupported on IBM i

--- Lib/test/test_signal.py
+++ Lib/test/test_signal.py
@@ -729,7 +729,7 @@ class ItimerTest(unittest.TestCase):
         self.assertEqual(self.hndl_called, True)
 
     # Issue 3864, unknown if this affects earlier versions of freebsd also
-    @unittest.skipIf(sys.platform in ('netbsd5',),
+    @unittest.skipIf(sys.platform in ('netbsd5', 'os400'),
         'itimer not reliable (does not mix well with threading) on some BSDs.')
     def test_itimer_virtual(self):
         self.itimer = signal.ITIMER_VIRTUAL
@@ -751,6 +751,8 @@ class ItimerTest(unittest.TestCase):
         # and the handler should have been called
         self.assertEqual(self.hndl_called, True)
 
+    @unittest.skipIf(sys.platform == 'os400',
+                     'ITIMER_PROF is unsupported on IBM i')
     def test_itimer_prof(self):
         self.itimer = signal.ITIMER_PROF
         signal.signal(signal.SIGPROF, self.sig_prof)
