aed60c83fa9e9b4e6cab76766d5358a74f9f9c39
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2021-03-24 11:32:10 -0500

    test: Skip test that requires CLOCK_THREAD_CPUTIME_ID
    CLOCK_THREAD_CPUTIME_ID is not supported on IBM i

--- Lib/test/test_time.py
+++ Lib/test/test_time.py
@@ -108,6 +108,8 @@ class TimeTestCase(unittest.TestCase):
                          'need time.pthread_getcpuclockid()')
     @unittest.skipUnless(hasattr(time, 'clock_gettime'),
                          'need time.clock_gettime()')
+    @unittest.skipIf(sys.platform == 'os400',
+                     'CLOCK_THREAD_CPUTIME_ID is unsupported on IBM i')
     def test_pthread_getcpuclockid(self):
         clk_id = time.pthread_getcpuclockid(threading.get_ident())
         self.assertTrue(type(clk_id) is int)
