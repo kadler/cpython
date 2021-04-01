a92fcb53f22d0329f9d0cd46db713d17396d3185
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2021-04-01 17:35:49 -0500

    WIP: Set int default handler even for SIG_IGN

--- Modules/signalmodule.c
+++ Modules/signalmodule.c
@@ -1437,7 +1437,7 @@ PyInit__signal(void)
             Handlers[i].func = Py_None; /* None of our business */
         Py_INCREF(Handlers[i].func);
     }
-    if (Handlers[SIGINT].func == DefaultHandler) {
+    if (Handlers[SIGINT].func == DefaultHandler || Handlers[SIGINT].func == IgnoreHandler) {
         /* Install default int handler */
         Py_INCREF(IntHandler);
         Py_SETREF(Handlers[SIGINT].func, IntHandler);
