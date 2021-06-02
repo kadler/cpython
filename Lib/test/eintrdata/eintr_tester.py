55a8636e0d657e0f03b07c922dfafd045361d944
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2021-06-02 16:04:42 -0500

    fixup! fixup! WIP: Skip tests in PASE that are skipped for AIX

--- Lib/test/eintrdata/eintr_tester.py
+++ Lib/test/eintrdata/eintr_tester.py
@@ -514,9 +514,6 @@ class FNTLEINTRTest(EINTRBaseTest):
                 self.stop_alarm()
             proc.wait()
 
-    # Issue 35633: See https://bugs.python.org/issue35633#msg333662
-    # skip test rather than accept PermissionError from all platforms
-    @unittest.skipIf(platform.system() in ("AIX", "OS400"), "AIX and OS400 returns PermissionError")
     def test_lockf(self):
         self._lock(fcntl.lockf, "lockf")
 
