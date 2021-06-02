99ae8b0e9d4a7ae46f5b939f5e3b201aa078106d
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2021-06-02 15:56:34 -0500

    fixup! fixup! test: Skip setting utime in 2108

--- Lib/test/test_importlib/source/test_file_loader.py
+++ Lib/test/test_importlib/source/test_file_loader.py
@@ -206,9 +206,10 @@ class SimpleTest(abc.LoaderTests):
             except OverflowError:
                 self.skipTest("cannot set modification time to large integer")
             except OSError as e:
-                if sys.platform == "os400" and e.errno == getattr(errno, 'EINVAL', None):
-                    self.skipTest("On IBM i IFS doesn't support times past 2038")
-                if e.errno != getattr(errno, 'EOVERFLOW', None):
+                errnos = [getattr(errno, 'EOVERFLOW', None)]
+                if sys.platform == 'os400':
+                    errnos.append(getattr(errno, 'EINVAL', None))
+                if e.errno not in errnos:
                     raise
                 self.skipTest("cannot set modification time to large integer ({})".format(e))
             loader = self.machinery.SourceFileLoader('_temp', mapping['_temp'])
